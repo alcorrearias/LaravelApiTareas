e68a02307e4eb2b9b961a967720e348e2f3c7e14
Ana <135356525+alcorrearias@users.noreply.github.com>
2024-09-23 20:36:33 -0500

    creando las rutas crud, y la bd de Tareas

--- ApiTareasCrud/routes/api.php
+++ ApiTareasCrud/routes/api.php
@@ -3,10 +3,23 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/users', function () {
-    return "Aca estoy";
+Route::get('/tareas', function () {
+    return "Aca estoy consultando mis tareas";
 });
+
+Route::get('/tarea/{id}', function () {
+    return "Aca estoy preguntando mi tarea";
+});
+
+Route::post('/tarea', function () {
+    return "Aca estoy guardando";
+});
+
+Route::put('/tarea/{id}', function () {
+    return "Aca estoy actualizando";
+});
+
+Route::delete('/tarea/{id}', function () {
+    return "Aca estoy borrando";
+});
+
